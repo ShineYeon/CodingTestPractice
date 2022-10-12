74d8a4d4e17a07a233564d058e317b49a6af33cf
ShineYeon <chlgmldus2@naver.com>
2022-10-12 22:35:43 +0900

    20056 완료

--- 20056.cpp
+++ 20056.cpp
@@ -4,98 +4,118 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
 
 using namespace std;
 
 int N;
 int board[21][21];
-bool visited[21][21];
+int dist[21][21];
 int sharkX, sharkY; //아기상어 위치 저장
 int sharkSize = 2; //아기상어 크기 저장
 
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
 
-int minLength = 98765432; //최단경로 저장
-vector<pair<int, int>> minLoc;  // 최단 경로에 있는 물고기 위치 저장
+int minX = -1, minY = -1;
+int minDist = 98765432; //최단경로 저장
+vector<pair<int, int>> minLoc; // 먹을 수 있는 물고기 위치 저장
 int ans = 0;
 int nowStack = 0; //현재까지 누적해서 먹은 물고기 수 저장
 
-bool cmp (pair<int, int> a, pair<int, int> b) {
-    if(a.first == b.first)
+bool cmp(pair<int, int> a, pair<int, int> b){
+    if(a.first==b.first)
         return a.second<b.second;
     else return a.first<b.first;
 }
 
-// destX, destY에 있는 물고기가 최단 경로인지 확인
-void minRoad(int startX, int startY, int destX, int destY, int cnt) {
-    if((startX == destX) && (startY==destY)) {
-        if(cnt < minLength) {
-            minLength = cnt;
-            minLoc.clear();
-            minLoc.push_back(make_pair(destX, destY));
-        }
-        else if(cnt==minLength) {
-            minLoc.push_back(make_pair(destX, destY));
-        }
-        return;
-    }
-    //visited[startX][startY] = true;
-    for(int i=0; i<4; i++) {
-        int newX = startX + dx[i];
-        int newY = startY + dy[i];
-        if(newX<0 || newX>=N || newY<0 || newY>=N || visited[newX][newY]) continue;
-        if(board[newX][newY] <= board[sharkX][sharkY]) {
-            visited[newX][newY] = true;
-            minRoad(newX, newY, destX, destY, cnt + 1);
-            visited[newX][newY] = false;
-        }
-    }
-}
+// 먹을 수 있는 물고기가 있는 경우 먹을 물고기를 선정 (minX, minY, minDist 갱신)
+void targeting() {
+    bool ret = false;
+    // 먹을 수 있는 물고기가 있을 때 타겟 선정
 
-// 최단경로 내에 있는 물고기 찾기
-bool findFish() {
-    bool check = false;
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
-            if(i==sharkX && j==sharkY) continue;
-            if(board[i][j] >= 1 && board[i][j]<=6 && (board[i][j] < board[sharkX][sharkY])){ //물고기 찾았으니까 최단경로 갱신해야 함
-                check = true;
-                minRoad(sharkX, sharkY, i, j, 0);
+            if(board[i][j] <sharkSize && board[i][j]!=0 && dist[i][j]!=-1 && dist[i][j] < minDist) {
+                minDist = dist[i][j];
+                minX = i, minY = j;
+                //ret = true;
             }
         }
     }
 
-    return check;
+    //return ret; // true 반환 시 물고기 선정된 것
 }
 
-// 찾은 곳으로 가서 먹기
-void go() {
-    sort(minLoc.begin(), minLoc.end(), cmp);
-    cout<<minLength<<"\n";
-    ans += minLength;
-    if(nowStack + 1 < sharkSize) {
-        nowStack+=1;
+// 지금 제일 가까운 공간에 먹을 수 있는 물고기 찾기
+void BFS() {
+
+    queue<pair<int, int>> q;
+    q.push(make_pair(sharkX, sharkY));
+
+    while(!q.empty()) {
+
+        int nowX = q.front().first;
+        int nowY = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int newX = nowX + dx[i];
+            int newY = nowY + dy[i];
+
+            if (newX < 0 || newX >= N || newY < 0 || newY >= N ||
+            sharkSize < board[newX][newY] || dist[newX][newY] != -1)
+                continue;
+
+            //거쳐가는 공간마다 거리 갱신, 거쳐간 공간을 distLoc으로 벡터에 저장
+            dist[newX][newY] = dist[nowX][nowY] + 1;
+            q.push(make_pair(newX, newY));
+
+            //if(sharkSize>board[newX][newY])
+            //    minLoc.push_back(make_pair(newX, newY));
+
+        }
+
     }
-    else if (nowStack + 1 == sharkSize) {
+}
+
+// 선정한 물고기 먹기
+bool eat() {
+    // minDist가 초기 98765432와 같은 수라면 갱신이 안 된 것임 (먹을 물고기가 없다는 뜻)
+    if(minDist == 98765432) return false;
+
+    sharkX = minX, sharkY = minY; // 해당 위치로 상어 이동
+    nowStack+=1; // 먹었으니까 이제까지 먹은 물고기 수 +1
+    board[minX][minY] = 0;
+    if(nowStack == sharkSize) { //이제까지 먹은 물고기 수가 상어 크기와 같다면 상어 사이즈 커지게 해야함
         nowStack = 0;
         sharkSize+=1;
     }
-    int newX = minLoc[0].first;
-    int newY = minLoc[0].second;
-    board[newX][newY] = 9;
-    board[sharkX][sharkY] = 0;
-    sharkX = newX, sharkY = newY;
+    ans+=minDist;
+    return true;
 }
 
 void sol() {
-    while(findFish()) {
-        go();
-        memset(visited, false, sizeof(visited));
-        minLength = 98765432;
-        minLoc.clear();
+    while(1) {
+
+        minDist = 98765432;
+        minX=-1, minY=-1;
+        memset(dist, -1, sizeof(dist));
+        dist[sharkX][sharkY] = 0;
+        //minLoc.clear();
+
+        BFS();
+
+        targeting();
+
+        if(!eat()) break;
+
     }
+
 }
+
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -105,7 +125,9 @@ int main() {
         for(int j=0; j<N; j++) {
             cin>>board[i][j];
             if(board[i][j] == 9){
+                board[i][j] = 0;
                 sharkX = i, sharkY=j;
+                sharkSize=2;
             }
         }
     }
